501cd16ffeda1f7cd74f57f550e89620aed46f64
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-04-22 01:51:59 -0300

    116. parte 18 - script da explosão

--- scenes/scripts/bomba.gd
+++ scenes/scripts/bomba.gd
@@ -4,8 +4,13 @@ var speed
 const GRAVITY_ACCEL = 1.03
 const LEVEL_UP_ADD = .1
 const PRE_FUMACA = preload("res://scenes/fumaca.tscn")
+const PRE_RASTRO = preload("res://scenes/rastro.tscn")
+const RASTRO_ACIMA_DA_BOMBA_EM_PIXELS = 30
+const TEMPO_ENTRE_RASTROS_EM_SEGUNDOS = 0.1
 
 func _ready():
+	$TimerRastro.wait_time = TEMPO_ENTRE_RASTROS_EM_SEGUNDOS
+	$TimerRastro.start()
 	speed = get_parent().get_bomba_speed()
 
 func _process(delta):
@@ -50,3 +55,11 @@ func mostra_fumaca():
 	get_parent().add_child(fumaca)
 	fumaca.play("default")
 	
+func mostra_rastro():
+	var rastro = PRE_RASTRO.instance()
+	rastro.position.x = position.x
+	rastro.position.y = position.y - RASTRO_ACIMA_DA_BOMBA_EM_PIXELS
+	get_parent().add_child(rastro)
+	
+func _on_TimerRastro_timeout():
+	mostra_rastro()
